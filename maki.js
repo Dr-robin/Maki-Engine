ddaff8543fc5c2d95803cf5cd172985dada55143
robin <qhecvjm20@naver.com>
2015-07-17 23:15:19 +0900

    설정 파일을 적당히 불러오는 기능을 추가했습니다

--- maki.js
+++ maki.js
@@ -1,6 +1,17 @@
-var express = require("express");
+var express = require("express"),
+	fs = require("fs");
 
 var app = express();
+var listModule = [];
+
+var config = {};
+if(fs.existsSync("config.json")) {
+	config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+	listModule = config.modules;
+}
+else {
+	console.log("설정파일을 찾을 수 없습니다.");
+}
 
 app.get("/", function (req, res) {
 	res.send("이게 다 마키 덕분입니다");
