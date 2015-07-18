f0c6cd554ac58a66871c2771609082d58c95315f
robin <qhecvjm20@naver.com>
2015-07-18 21:23:10 +0900

    적당히 구조를 만들었습니다

--- maki.js
+++ maki.js
@@ -1,20 +1,34 @@
 var express = require("express"),
-	fs = require("fs");
+	fs = require("fs"),
+	mysql = require("node-mysql");
 
 var app = express();
 var listModule = [];
+var sql;
+var tablePrefix = "";
+var isUsable = false; // 설치되지 않았으면 false
 
 var config = {};
 if(fs.existsSync("config.json")) {
 	config = JSON.parse(fs.readFileSync("config.json", "utf8"));
-	listModule = config.modules;
+	if(config.version >= 1) {
+		sql = mysql.createPool({connectionLimit: config.sql.limit, host: config.sql.host, user: config.sql.user, password: config.sql.password, database: config.sql.database});
+		tablePrefix = config.sql.prefix;
+		isUsable = true;
+	}
+	else {
+		console.log("설정파일이 잘못되었습니다.");
+	}
 }
 else {
 	console.log("설정파일을 찾을 수 없습니다.");
 }
 
 app.get("/", function (req, res) {
-	res.send("이게 다 마키 덕분입니다");
+	res.send("Hello, World!");
+});
+app.get("/:module", function (req, res) {
+	res.send(req.params.module);
 });
 
 app.listen(80, function () {
