3b4cd48f1d2856d11a08eedc821118a489bfc39f
robin <qhecvjm20@naver.com>
2015-07-21 22:07:54 +0900

    모듈 로드 함수의 위치랑 줄바꿈 처리가 잘못되 있어서 수정했습니다

--- maki.js
+++ maki.js
@@ -6,15 +6,25 @@ var express = require("express"),
 var app = express();
 var listModule = {};
 var sql;
-var tablePrefix = "";
 var isUsable = false; // 설치되지 않았으면 false
+
+global.app = app;
+global.loadModule = function(moduleName) {
+	if(!listModule[moduleName]) {
+		listModule[moduleName] = require("./module/" + moduleName + "/module.js");
+	}
+	return listModule[moduleName];
+};
+
+app.set("view engine", "ejs");
 
 var config = {};
 if(fs.existsSync("config.json")) {
 	config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 	if(config.version >= 1) {
 		sql = mysql.createPool({connectionLimit: config.sql.limit, host: config.sql.host, user: config.sql.user, password: config.sql.password, database: config.sql.database});
-		tablePrefix = config.sql.prefix;
+		global.sql = sql;
+		global.sqlPrefix = config.sql.prefix;
 		isUsable = true;
 	}
 	else {
@@ -25,17 +35,6 @@ else {
 	console.log("설정파일을 찾을 수 없습니다.");
 	global.loadModule("setup");
 }
-
-app.set("view engine", "ejs");
-
-global.app = app;
-global.sql = sql;
-global.loadModule = function(moduleName) {
-	if(!listModule[moduleName]) {
-		listModule[moduleName] = require("./module/" + moduleName + "/module.js");
-	}
-	return listModule[moduleName];
-};
 
 app.listen(80, function() {
 	console.log("Running...");
